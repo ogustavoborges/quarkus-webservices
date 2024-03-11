a2f8abf42e671ea69d08cae3302385d3e2e07652
Gustavo Borges <81187140+ogustavoborges@users.noreply.github.com>
2024-03-11 13:51:43 +0000

    aula-01

--- webservices/src/main/java/edu/ifrs/GreetingResource.java
+++ webservices/src/main/java/edu/ifrs/GreetingResource.java
@@ -1,16 +1,38 @@
 package edu.ifrs;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/api")
 public class GreetingResource {
 
     @GET
+    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello from RESTEasy";
+    }
+
+    @POST
+    @Path("/helloPost")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello2(@FormParam("name") String name){
+            return "{\"message\": \"Hello " + name + "\"}";
+    }
+
+    
+    @GET
+    @Path("/helloGet/{name}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello3(@PathParam("name") String name){
+            return "{\"message\": \"Hello " + name + "\"}";
     }
 }
--- webservices/src/test/java/edu/ifrs/GreetingResourceTest.java
+++ webservices/src/test/java/edu/ifrs/GreetingResourceTest.java
@@ -6,15 +6,39 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import java.beans.Transient;
+
 @QuarkusTest
 class GreetingResourceTest {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/api/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .body(is("Hello from RESTEasy"));
+    }
+
+
+    @Test
+    void testHello2Endpoint() {
+        given()
+            .formParam("name", "John")
+            .when().post("/api/helloPost")
+            .then()
+            .statusCode(200)
+            .body(is("{\"message\": \"Hello John\"}"));
+    }
+
+
+
+    @Test
+    void testHello3Endpoint() {
+        given()
+          .when().get("/api/helloGet/John")
+          .then()
+             .statusCode(200)
+             .body(is("{\"message\": \"Hello John\"}"));
     }
 
 }
