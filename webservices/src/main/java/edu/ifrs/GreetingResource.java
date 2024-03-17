c9b6ed269fadd70717f5f6e911bf70f2ab2aca0c
Gustavo <gustavoob06@gmail.com>
2024-03-17 18:53:32 -0300

    exercicio 1

--- webservices/src/main/java/edu/ifrs/GreetingResource.java
+++ webservices/src/main/java/edu/ifrs/GreetingResource.java
@@ -3,11 +3,12 @@ package edu.ifrs;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.core.Response;
+import jakarta.json.Json;
 
 @Path("/api")
 public class GreetingResource {
@@ -35,4 +36,39 @@ public class GreetingResource {
     public String hello3(@PathParam("name") String name){
             return "{\"message\": \"Hello " + name + "\"}";
     }
+
+    //Exercício 2
+
+    // @POST
+    // @Path("/km-to-miles")
+    // @Consumes(MediaType.TEXT_PLAIN)
+    // @Produces(MediaType.TEXT_PLAIN)
+    // public String kmphToMph(double kmph) {
+    //     double mph = kmph * 0.621371;
+    //     // Truncate to 5 decimal places
+    //     mph = Math.floor(mph * 100000) / 100000;
+    //     return Double.toString(mph);
+    // }
+
+        // @POST
+        // @Path("/km-to-miles")
+        // @Consumes(MediaType.TEXT_PLAIN)
+        // @Produces(MediaType.TEXT_PLAIN)
+        // public String conversionKmMiles(double km){
+        //     double miles = km  * 0.621371;
+        //     miles = Math.floor(miles * 100000) / 100000;
+        //     return Double.toString(miles);
+        // }
+
+        // @GET
+        // @Path("/knots-to-km/{knots}")
+        // @Produces(MediaType.APPLICATION_JSON)
+        // public Response knotsToKmph(@PathParam("knots") double knots) {
+        //     double kmph = knots * 1.852;
+        //     return Response.ok(Json
+        //         .createObjectBuilder()
+        //         .add("value", kmph)
+        //         .build()).build();
+        // }
+
 }
--- webservices/src/test/java/edu/ifrs/GreetingResourceTest.java
+++ webservices/src/test/java/edu/ifrs/GreetingResourceTest.java
@@ -7,6 +7,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 import java.beans.Transient;
+import io.restassured.http.ContentType;
 
 @QuarkusTest
 class GreetingResourceTest {
@@ -19,7 +20,6 @@ class GreetingResourceTest {
              .body(is("Hello from RESTEasy"));
     }
 
-
     @Test
     void testHello2Endpoint() {
         given()
@@ -30,8 +30,6 @@ class GreetingResourceTest {
             .body(is("{\"message\": \"Hello John\"}"));
     }
 
-
-
     @Test
     void testHello3Endpoint() {
         given()
@@ -40,5 +38,46 @@ class GreetingResourceTest {
              .statusCode(200)
              .body(is("{\"message\": \"Hello John\"}"));
     }
+
+
+    //Exercício 2
+
+    /**
+     * Test case for converting kilometers to miles.
+     *
+     * This test sends a POST request to the "/Conversion/km-to-miles"
+     * endpoint with a body of "50" (representing 50 kilometers per hour).
+     * The expected result is a response with a status code of 200 and a
+     * body of "31.06855" (the equivalent value in miles per hour).
+    */
+    // @Test
+    // void testConversionKmMiles() {
+    //     given()
+    //         .contentType(ContentType.TEXT)
+    //         // 50 quilômetros por hora
+    //         .body("50")
+    //     .when()
+    //         .post("/Conversion/km-to-miles")
+    //     .then()
+    //         .contentType(ContentType.TEXT)
+    //         .statusCode(200)
+    //         .body(is("31.06855"));
+    // }
+
+    /**
+     * Test case to verify the conversion from knots to kilometers per hour.
+     * The expected value for 1 knot in km/h is 1.852.
+    */
+    // @Test
+    // void testConversionKnotsKm() {
+    //     given()
+    //         .contentType(ContentType.TEXT)
+    //     .when()
+    //         .contentType(ContentType.JSON)
+    //         .get("/Conversion/knots-to-km/1")
+    //     .then()
+    //         .statusCode(200)
+    //         .body("value", is(1.852f));
+    // }
 
 }
